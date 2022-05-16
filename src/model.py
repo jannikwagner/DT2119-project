4e26fe16e11b216a6e077f272135d3169babd2e9
Jannik Wagner <jannik.wagner@students.uni-mainz.de>
2022-05-17 00:47:04 +0200

    added model, not tested

--- src/model.py
+++ src/model.py
@@ -7,6 +7,17 @@ import torch.nn.functional as F
 Probably there should be different files for different models.
 """
 
+def get_model(config, data_dim):
+    if config.model_type == "simple_vae":
+        model = SimpleVariationalAutoencoder(data_dim, config.latent_dim, config.device)
+    
+    elif config.model_type == "linear_vae":
+        model = LinearVariationalAutoencoder(data_dim, config.latent_dim, config.hidden_dims, config.device)
+
+    elif config.model_type == "conv2_vae":
+        model = conv2d_builder(data_dim, config.latent_dim, config.device, config.channels, config.strides, config.kernel_sizes, config.paddings, config.nonlinearity, config.batch_norm)
+    return model.to(config.device)
+
 
 def mc_kl_divergence(z, z_mu, z_std):
     # stolen from https://towardsdatascience.com/variational-autoencoder-demystified-with-pytorch-implementation-3a06bee395ed
@@ -50,7 +61,7 @@ class SimpleVariationalEncoder(nn.Module):
         self.seq = nn.Sequential(*layers)
 
         self.fc_z_mu = nn.Linear(512, latent_dim)
-        self.fc_z_log_var = nn.Linear(512, latent_dim)
+        self.fc_z_log_var = F.tanh(nn.Linear(512, latent_dim))
         
         self.N = torch.distributions.Normal(0, 1)
         self.N.loc = self.N.loc.to(device)
@@ -103,12 +114,13 @@ class SimpleVariationalAutoencoder(nn.Module):
         return self.decoder(z), kl
 
 class LinearBlock(nn.Module):
-    def __init__(self, in_dim, out_dim, batch_norm=True, activation_fn=nn.ReLU):
+    def __init__(self, in_dim, out_dim, batch_norm=True, nonlinearity="relu"):
         super().__init__()
+        nonlinearity = NONLINEARITIES[nonlinearity]
         layers = [
             nn.Linear(in_dim, out_dim),
             nn.BatchNorm1d(out_dim) if batch_norm else nn.Identity(),
-            activation_fn()
+            nonlinearity
         ]
         self.seq = nn.Sequential(*layers)
     def forward(self, x):
@@ -148,7 +160,7 @@ class LinearVariationalEncoder(nn.Module):
         x = self.flatten(x)
         x = self.seq(x)
         z_mu = self.fc_z_mu(x)
-        z_log_var = F.tanh(self.fc_z_log_var(x))
+        z_log_var = self.fc_z_log_var(x)
         z_var = torch.exp(z_log_var)
         z_std = torch.exp(z_log_var/2)
         z = z_mu + z_std*self.N.sample(z_mu.shape)
@@ -189,11 +201,144 @@ class LinearVariationalAutoencoder(nn.Module):
         z, kl = self.encoder(x)
         return self.decoder(z), kl
 
-def get_model(config, data_dim):
-    if config.model_type == "simple_vae":
-        model = SimpleVariationalAutoencoder(data_dim, config.latent_dim, config.device)
-    
-    elif config.model_type == "linear_vae":
-        model = LinearVariationalAutoencoder(data_dim, config.latent_dim, config.hidden_dims, config.device)
 
-    return model.to(config.device)
+NONLINEARITIES = {
+    "relu" : nn.ReLU(),
+    "tanh" : nn.Tanh(),
+    None : nn.Identity()
+}
+
+def update_h(h, p, k, s):
+    return (h+2*p-(k-1)-1) // s + 1
+def update_chw(h, w, c2, p, k, s):
+    return c2, update_h(h, p, k, s), update_h(w, p, k, s)
+
+class Conv2dStack(nn.Module):
+    def __init__(self, data_dim, channels, strides=None, kernel_sizes=None, paddings=None, nonlinearity="relu", batch_norm=True):
+        super().__init__()
+        if strides is None: strides = [1] * len(channels)
+        if kernel_sizes is None: kernel_sizes = [3] * len(channels)
+        if paddings is None: paddings = [0] * len(channels)
+        nonlinearity = NONLINEARITIES[nonlinearity]
+        *_, c, h, w = data_dim
+        print(c, h, w)
+
+        layers = []
+        for c2, k, s, p in channels, kernel_sizes, strides, paddings:
+            layers.append(nn.Conv2d(c, c2, k, s, p))
+            layers.append(nonlinearity)
+            if batch_norm:
+                layers.append(nn.BatchNorm1d(c2))
+            c, h, w = update_chw(h, w, c2, p, k, s)
+        
+        layers.append(nn.Flatten())
+
+        self.seq = nn.Sequential(*layers)
+
+        self.dim = torch.prod([c, h, w])
+    def forward(self, x):
+        x = self.seq(x)
+        return x
+
+class TransposedConv2dStack(nn.Module):
+    def __init__(self, data_dim, channels, strides=None, kernel_sizes=None, paddings=None, nonlinearity="relu", batch_norm=True):
+        super().__init__()
+        if strides is None: strides = [1] * len(channels)
+        if kernel_sizes is None: kernel_sizes = [3] * len(channels)
+        if paddings is None: paddings = [0] * len(channels)
+        nonlinearity = NONLINEARITIES[nonlinearity]
+        *_, c, h, w = data_dim
+        print(c, h, w)
+
+        layers = []
+        for c2, k, s, p in channels, kernel_sizes, strides, paddings:
+            if batch_norm:
+                layers.append(nn.BatchNorm1d(c2))
+            layers.append(nonlinearity)
+            layers.append(nn.ConvTranspose2d(c2, c, k, s, p))
+            c, h, w = update_chw(h, w, c2, p, k, s)
+        
+        layers.append(nn.Unflatten((c, h, w)))
+
+        layers = list(reversed(layers))
+
+        self.seq = nn.Sequential(*layers)
+
+        self.dim = torch.prod([c, h, w])
+    def forward(self, x):
+        x = self.seq(x)
+        return x
+
+class Encoder(nn.Module):
+    def __init__(self, stack, latent_dim):
+        super().__init__()
+        self.stack = stack
+        dim = stack.dim
+
+        self.fc_z_mu = nn.Linear(dim, latent_dim)
+        self.fc_z_log_var = F.tanh(nn.Linear(dim, latent_dim))
+    
+    def forward(self, x):
+        #x = torch.flatten(x, start_dim=1)
+        # x = torch.flatten(x, 1)
+        x = self.stack(x)
+        z_mu = self.fc_z_mu(x)
+        z_log_var = self.fc_z_log_var(x)
+        return z_mu, z_log_var
+
+
+class Decoder(nn.Module):
+    def __init__(self, stack, latent_dim):
+        super().__init__()
+        self.stack = stack
+        dim = stack.dim
+
+        self.fc = nn.Linear(latent_dim, dim)
+        
+    def forward(self, z):
+        z = self.stack(z)
+        x_rec = self.fc(z)
+        return x_rec
+
+
+class VariationalAutoencoder(nn.Module):
+    def __init__(self, encoder, decoder, latent_dim, device):
+        super(VariationalAutoencoder, self).__init__()
+        self.latent_dim = latent_dim
+        self.encoder = encoder
+        self.decoder = decoder
+        
+        self.N = torch.distributions.Normal(0, 1)
+        self.N.loc = self.N.loc.to(device)
+        self.N.scale = self.N.scale.to(device)
+
+    def forward(self, x):
+        z_mu, z_log_var = self.encoder(x)
+
+        z_std = torch.exp(z_log_var/2)
+        z = z_mu + z_std*self.N.sample(z_mu.shape)
+        kl = kl_divergence(z_mu, z_log_var)
+
+        x_rec = self.decoder(z)
+        return x_rec, kl
+
+    def sample(self, n):
+        self.N.sample((n, self.latent_dim))
+
+    def to(self, device, *args):
+        old_device = self.N.loc.device
+        self.N.loc = self.N.loc.to(device)
+        self.N.scale = self.N.scale.to(device)
+        model = super().to(device, *args)
+
+        self.N.loc = self.N.loc.to(old_device)
+        self.N.scale = self.N.scale.to(old_device)
+        return model
+        
+def conv2d_builder(data_dim, latent_dim, device, channels, strides=None, kernel_sizes=None, paddings=None, nonlinearity="relu", batch_norm=True):
+    encoder_stack = Conv2dStack(data_dim, channels, strides, kernel_sizes, paddings, nonlinearity, batch_norm)
+    decoder_stack = TransposedConv2dStack(data_dim, channels, strides, kernel_sizes, paddings, nonlinearity, batch_norm)
+    encoder = Encoder(encoder_stack, latent_dim)
+    decoder = Decoder(decoder_stack, latent_dim)
+    vae = VariationalAutoencoder(encoder, decoder, latent_dim, device)
+    return vae
