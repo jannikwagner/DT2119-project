43e0544c802d1fc4db2c98dd87e5cd44f830ec39
Erik Henriksson <erik.ch.henriksson@gmail.com>
2022-05-18 15:11:04 +0200

    make_data_figs for making images of waveforms and mel_spec

--- src/make_data_figs.py
+++ src/make_data_figs.py
@@ -1,17 +1,33 @@
 import unittest
+import wave
 from data import DataManager
 from configuration import Config
 import os
 import matplotlib.pyplot as plt
+import numpy as np
 
-FIG_PATH = "../fig"
+import librosa
+import librosa.display
 
-configuration_path = 'configurations' + os.sep + 'exp10.yaml'
+FIG_PATH = "../fig/"
+
+configuration_path = 'configurations' + os.sep + 'exp_draw.yaml'
 config = Config(configuration_path)
 data_manager = DataManager(config, print_info=False)
-some_utterances = [0] #[0, 25, 123, 220]
+some_utterances = [0, 5000, 10000, 20000, 40000]
+sr = 16000
 for i in some_utterances:
-    print()
-    """ mel_spec = data_manager.transform(data_manager.train_set[i][0])[0]
-    plt.pcolormesh(mel_spec)
-    plt.show() """
+    waveform, sample_rate, label, speaker_id, utterance_number = data_manager.train_set[i]
+    waveform = np.array(waveform)
+    librosa.display.waveshow(waveform, sr=sr)
+    plt.title("Waveform for utterance " + label)
+    plt.savefig(FIG_PATH+label+'_'+speaker_id+'_waveform.png')
+    plt.cla()
+    S = librosa.feature.melspectrogram(waveform, sr=sr, n_fft=config.n_fft, hop_length=config.hop_length, n_mels=config.n_mels)
+    S_DB = librosa.power_to_db(S, ref=np.max)[0]
+    librosa.display.specshow(S_DB, sr=sr, hop_length=256, x_axis='time', y_axis='mel')
+    cb = plt.colorbar(format='%+2.0f dB')
+    plt.title("Mel Spectogram for utterance " + label)
+    plt.savefig(FIG_PATH+label+'_'+speaker_id+'_melspec.png')
+    cb.remove()
+    plt.cla()
