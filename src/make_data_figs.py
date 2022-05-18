f5421b3fcb28f7f608bfcecb749f475a89030e08
Erik Henriksson <erik.ch.henriksson@gmail.com>
2022-05-18 16:59:46 +0200

    update make_data_figs

--- src/make_data_figs.py
+++ src/make_data_figs.py
@@ -14,20 +14,22 @@ FIG_PATH = "../fig/"
 configuration_path = 'configurations' + os.sep + 'exp_draw.yaml'
 config = Config(configuration_path)
 data_manager = DataManager(config, print_info=False)
-some_utterances = [0, 5000, 10000, 20000, 40000]
+some_utterances = [0, 1000, 5000, 6000, 10000, 11000, 20000, 21000]
 sr = 16000
 for i in some_utterances:
     waveform, sample_rate, label, speaker_id, utterance_number = data_manager.train_set[i]
     waveform = np.array(waveform)
-    librosa.display.waveshow(waveform, sr=sr)
-    plt.title("Waveform for utterance " + label)
+    """ librosa.display.waveshow(waveform, sr=sr)
     plt.savefig(FIG_PATH+label+'_'+speaker_id+'_waveform.png')
-    plt.cla()
+    plt.cla() """
     S = librosa.feature.melspectrogram(waveform, sr=sr, n_fft=config.n_fft, hop_length=config.hop_length, n_mels=config.n_mels)
     S_DB = librosa.power_to_db(S, ref=np.max)[0]
-    librosa.display.specshow(S_DB, sr=sr, hop_length=256, x_axis='time', y_axis='mel')
+    librosa.display.specshow(S_DB, sr=sr, hop_length=256)
     cb = plt.colorbar(format='%+2.0f dB')
-    plt.title("Mel Spectogram for utterance " + label)
+    cb.ax.tick_params(labelsize=18) 
+    plt.xlabel('time', fontsize=18)
+    plt.ylabel('mel', fontsize=18)
+    plt.title(label, fontsize=23)
     plt.savefig(FIG_PATH+label+'_'+speaker_id+'_melspec.png')
     cb.remove()
     plt.cla()
